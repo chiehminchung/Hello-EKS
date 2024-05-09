da08f03dc4ada98d2d3582f189306359d36943ee
chiehminchung <chiehminchung@gmail.com>
2024-05-09 17:23:20 -0400

    "/greeting"

--- src/main/java/org/example/helloeks/HelloEksApplication.java
+++ src/main/java/org/example/helloeks/HelloEksApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class HelloEksApplication {
-    @GetMapping
+    @GetMapping("/greeting")
     public String message(){
         return "Hello EKS";
     }
